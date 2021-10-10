fdc9cbfcf4b44486f4ed4c3d0a6a7c35610edd47
Андрей Зубрицкий <andrejzubrickij@MacBook-Pro-Andrej.local>
2021-10-10 23:12:53 +0300

    2nd commit

---
+++ cmake-build-debug/CMakeFiles/lad5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lad5.dir/main.cpp.o"
+  "lad5"
+  "lad5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lad5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
